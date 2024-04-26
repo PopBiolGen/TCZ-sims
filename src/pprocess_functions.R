181fe6faac069291d676a78e71bca5b647a4be19
Ben Phillips <ben.l.phillips@curtin.edu.au>
2024-04-26 14:24:51 +0800

    move setup to functions

--- src/pprocess_functions.R
+++ src/pprocess_functions.R
@@ -151,6 +151,50 @@ obs_pred_cf<-function(preds, obs){
 	sum(temp)
 }
 
+# Sets up the spread table and pulls parameters ready for simulations
+  # returns the spread table
+setup <- function() {
+  source("src/pprocess_functions.R")
+  load("dat/Kernel_fits.RData")
+  plb<-read.csv("dat/art_nat_clp.csv")
+  load("dat/Posteriors.RData")
+  
+  #max(plb$POINT_X)-min(plb$POINT_X) # 436252.5
+  #max(plb$POINT_Y)-min(plb$POINT_Y) # 318785.0
+  pairs_pdist<-pdist.fast(X=plb$POINT_X,Y=plb$POINT_Y,maximum=500000,space.size= 500000)
+  
+  #get matrix for the 'spread' function
+  # need matrix containing:
+  # "ID, X, Y, Pres (0s), n.pairs, u (rainy days*85.35[which is estimate of u]), 
+  # age (0s)"
+  
+  ID<-as.numeric(rownames(plb))
+  X<-plb$POINT_X
+  Y<-plb$POINT_Y
+  Pres<-plb$ARRIVE_MCP
+  age<-rep(0,length(X))
+  target<-which(Pres==2)
+  Pres[Pres==2]<-0
+  nats<-which(plb$art_nat==0)
+  arts<-which(plb$art_nat==1)
+  
+  
+  # calculate n.pairs using pdist
+  n.pairs<-do.call("c",lapply(pairs_pdist,nrow))
+  
+  u<-(plb$rain_1mm-1)/364
+  u<-3*(u-u^2) + u^3
+  u<-plb$rain_1mm+3*plb$rain_1mm*(1-u)
+  u<-floor(u)
+  load("dat/Kernel_fits.RData")
+  
+  u<-fits[u,1:2]
+  
+  assign("spread.table", 
+         as.matrix(cbind(ID,X,Y,Pres,n.pairs,u,age),nrow=length(age),ncol=7),
+         envir = .GlobalEnv)
+}
+
 
 # spreads the population over gens generations or until target sites are reached
 # returns number of generations

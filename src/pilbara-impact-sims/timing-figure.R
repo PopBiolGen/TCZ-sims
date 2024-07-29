9eb3c623e4062fb00c3a65c19405d50bc9117a0a
Ben Phillips <ben.l.phillips@curtin.edu.au>
2024-07-29 19:24:25 +0800

    Finalise Pilbara timing figure

--- src/pilbara-impact-sims/timing-figure.R
+++ src/pilbara-impact-sims/timing-figure.R
@@ -1,9 +1,17 @@
-load("out/timing-to-pilbara_likely-case.RData")
+load("out/do-nothing.RData")
 
 time.vec <- unlist(lapply(output, FUN = function(x){c(x$gen)}))
 time.vec <- time.vec + 1 # bring to 2023 start date
 arrival.year <- 2023+time.vec
 
-pdf(file = "out/timing-to-pilbara_likely-case_boxplot.pdf")
-  boxplot(arrival.year, horizontal = TRUE, xlab = "Arrival year")
+other.ests <- c(2035, 2041) # arrival estimates from extrapolation methods (see appendix 1)
+
+pdf(file = "out/timing-to-pilbara_likely-case_boxplot.pdf", width = 10, height = 5)
+  boxplot(arrival.year, 
+          horizontal = TRUE, 
+          xlab = "Arrival year",
+          ylim = c(2030, max(arrival.year)))
+  points(other.ests, rep(1, 2), pch = c(17, 19))
+  text(x = c(other.ests), y = rep(1,2), labels = c("Constant speed", "Rain-dependent speed"), pos = 1)
+  text(x = median(arrival.year), y = 0.75, labels = c("Stochastic model"))
 dev.off()

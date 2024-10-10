b0fcc2b1e245daef19ae48bcf0d5b9e77088a6c0
Ben Phillips <ben.l.phillips@curtin.edu.au>
2024-10-10 08:18:37 +0800

    add cluster shell script for setup

---
+++ src/cluster/a-setup-environment.sh
@@ -0,0 +1,11 @@
+# Project directory
+cd $MYSCRATCH/TCZ-sims
+
+# Module required for R
+module load r/4.3.0
+
+# Modules required for sf
+module load gdal/3.7.3-ztwnpws
+module load geos/3.12.0-wntgupl
+module load .proj/9.2.1-svjrii5
+module load .udunits/2.2.28-lffxtmr

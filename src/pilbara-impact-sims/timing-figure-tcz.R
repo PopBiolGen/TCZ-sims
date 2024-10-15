b1e4d4559add61db2fde9e3d332114ab361a07a6
Ben Phillips <ben.l.phillips@curtin.edu.au>
2024-10-15 09:23:59 +0800

    make boxplot figure for timing of arrival at tcz

---
+++ src/pilbara-impact-sims/timing-figure-tcz.R
@@ -0,0 +1,19 @@
+load("out/tcz-arrival.RData")
+
+time.vec <- unlist(lapply(output, FUN = function(x){c(x$gen)}))
+time.vec <- time.vec + 1 # bring to 2023 start date
+arrival.year <- 2023+time.vec
+
+source("src/pilbara-impact-sims/spread-rate-rainfall.R") # estimate arrival time taking into account only variation in rainfall
+
+other.ests <- c(test.d/43, tcz.tt) # arrival estimates from extrapolation methods (see pilbara impact paper Appendix 1 for details)
+
+pdf(file = "out/timing-to-tcz_boxplot.pdf", width = 10, height = 5)
+  boxplot(arrival.year, 
+          horizontal = TRUE, 
+          xlab = "Arrival year",
+          ylim = c(2030, max(arrival.year)))
+  points(other.ests, rep(1, 2), pch = c(17, 19))
+  text(x = c(other.ests), y = rep(1,2), labels = c("Constant speed", "Rain-dependent speed"), pos = 1)
+  text(x = median(arrival.year), y = 0.75, labels = c("Stochastic model"))
+dev.off()
